55f24868affaf14125f285b181c40d7b4358b491
maivaswrld <113538524+maivaswrld@users.noreply.github.com>
2023-02-21 11:18:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var superheroName = superheroes.random();
+var supervillainName = supervillains.random()
+
+console.log(supervillainName);
